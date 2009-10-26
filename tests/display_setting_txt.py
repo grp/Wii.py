4209dc2cbd23ebd9cc5859d92d67e32f2b3a2bed
megazig <megazig@hotmail.com>
2009-10-26 04:45:38 -0500

    added usage to setting.txt display

--- tests/display_setting_txt.py
+++ tests/display_setting_txt.py
@@ -3,6 +3,9 @@
 import sys, Wii
 
 def main():
+	if len(sys.argv) == 1:
+		print "python display_setting_txt.py <setting.txt>"
+		exit(1)
 	settings = Wii.CONF(sys.argv[1])
 	print settings.getKeysName()
 	for x in settings.getKeysName():
