6978355656ac73aa73ef1eea0f52c50644010b56
Stephen Simpson <megazig@gmail.com>
2011-10-30 23:20:51 -0500

    Had some info prints left in and added newline at end of .py file

--- NUStool.py
+++ NUStool.py
@@ -8,13 +8,19 @@
 import Wii, sys, os, shutil
 
 def downloadPack(titleid, version, outDir, pack):
+	print "downloadPack(0x%016x, %d, %s, %d)" % (titleid,version,outDir,pack)
 	if version != 0:
+		print "downloading....",
 		Wii.NUS.download(titleid, version).dumpDir(outDir)
 	else:
+		print "downloading....",
 		Wii.NUS.download(titleid).dumpDir(outDir)
+	print "done"
 	if pack == True:
+		print "packing........",
 		Wii.WAD.loadDir(outDir).dumpFile(str(outDir+".wad"))
 		shutil.rmtree(outDir)
+		print "done"
 
 if len(sys.argv) % 3 == 2:
 	pack, x = False, 1
@@ -23,6 +29,9 @@ else:
 
 for i in range(1 + x, len(sys.argv), 3):
 	titleid = int(sys.argv[i], 16)
+	print "TitleId: %016x" % titleid
 	version = int(sys.argv[i+1])
+	print "Version: %d" % version
 	outDir = sys.argv[i+2]
 	downloadPack(titleid, version, outDir, pack)
+
